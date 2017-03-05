33cdb64c119cfdc514581eb1d9a6cf64c916ecc2
RADJAIYoucef <radjai--youcef@hotmail.com>
2017-03-05 21:52:20 +0100

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,24 @@
+	function cacher1 (Formation) {
+	$("#Formation").hide();
+	}
+	function afficher1 (Formation) {
+	$("#Formation").show();
+	}
+	function cacher2 (Experience_Pro) {
+	$("#Experience_Pro").hide();
+	}
+	function afficher2 (Experience_Pro) {
+	$("#Experience_Pro").show();
+	}
+	function cacher3 (Compétence) {
+	$("#Compétence").hide();
+	}
+	function afficher3 (Compétence) {
+	$("#Compétence").show();
+	}
+	function cacher4 (Linguistiques) {
+	$("#Linguistiques").hide();
+	}
+	function afficher4 (Linguistiques) {
+	$("#Linguistiques").show();
+	}
